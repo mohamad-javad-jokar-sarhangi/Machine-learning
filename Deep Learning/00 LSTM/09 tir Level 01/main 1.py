e803967fc91bc23daa8084bdb3298e75b257c5af
mohamad-javad-jokar-sarhangi <mjavadx3@gmail.com>
2025-07-03 23:37:10 +0330

    finish Level 1 LSTM

--- Deep Learning/00 LSTM/09 tir Level 01/main 1.py
+++ Deep Learning/00 LSTM/09 tir Level 01/main 1.py
@@ -19,27 +19,34 @@ random.seed(42)
 
 ### Part 1 Ready Data
 # --- 1. بارگذاری دیتا و تبدیل تاریخ ---
+# 01
 df = pd.read_csv(
     r'Deep Learning\00 LSTM\09 tir Level 01\XAUUSD D1 2008-08-08 to 2025-04-18.csv',
     sep='	',
     header=None,
     names=['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
 )
-
+# 02
 df['Date'] = pd.to_datetime(df['Date'])
+
+# 03
 df = df.sort_values('Date').reset_index(drop=True)
 
+# 04
 # فقط ستون‌های مورد نیاز (می‌تونی فیچر اضافه کنی)
 feat_cols = ['Open', 'High', 'Low', 'Close', 'Volume']
 data = df[feat_cols].copy()
 
+# 05
 # --- 2. نرمال‌سازی ---
 scaler = MinMaxScaler()
 data[feat_cols] = scaler.fit_transform(data[feat_cols])
 
+# 06
 # --- 3. ساخت X و y به صورت پنجره‌ای ---
 window = 5
 
+# 07
 X, y = [], []
 for i in range(window, len(data)):
     X.append(data.iloc[i-window:i].values)   # سکانس روزهای گذشته
@@ -47,12 +54,14 @@ for i in range(window, len(data)):
 
 X, y = np.array(X), np.array(y)    # X.shape=(نمونه, window, ویژگی)
 
+# 08
 # --- 4. تقسیم بر اساس زمان (نه تصادفی) ---
 split_index = int(0.8 * len(X))
 X_train, X_test = X[:split_index], X[split_index:]
 y_train, y_test = y[:split_index], y[split_index:]
 
 ### Part 2 Create LSTM with Keras
+# 01
 # --- 1. تعریف مدل ---
 model = Sequential()
 model.add(LSTM(64, input_shape=(window, len(feat_cols)), return_sequences=False))
@@ -62,16 +71,20 @@ model.add(Dense(1))  # خروجی عددی
 model.compile(loss='mse', optimizer='adam')
 
 # --- 2. آموزش مدل ---
+# 02
 history = model.fit(X_train, y_train, epochs=35, batch_size=32, validation_split=0.1)
 
 # --- 3. ارزیابی ---
+# 03
 loss = model.evaluate(X_test, y_test)
 print('Test MSE:', loss)
 
 # --- 4. پیش‌بینی و بازگرداندن قیمت به حالت اصلی ---
+# 04
 y_pred = model.predict(X_test)
 
 # بازیابی اسکیل برای مشاهده عدد واقعی
+# 04
 y_test_rescaled = scaler.inverse_transform(
     np.concatenate((np.zeros((len(y_test), 4)), y_test.reshape(-1,1)), axis=1)
 )[:,-1]
@@ -79,6 +92,7 @@ y_pred_rescaled = scaler.inverse_transform(
     np.concatenate((np.zeros((len(y_pred), 4)), y_pred), axis=1)
 )[:,-1]
 
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 ### Part 3 Show Result
 plt.figure(figsize=(12,5))
 plt.plot(y_test_rescaled, label='Real', color='red')
